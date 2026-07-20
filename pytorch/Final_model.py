649f3692e4592163d756933cd37ff4f78c2a8c64
Zion Onwujuba <ziononwujuba233@gmail.com>
2026-07-20 13:38:53 -0500

    Exported Gini Model PTE, created C library for freertos

--- pytorch/Final_model.py
+++ pytorch/Final_model.py
@@ -1,28 +1,19 @@
 import torch
-import torch.nn as nn
-from torch.utils.data import DataLoader, TensorDataset
 import numpy as np
 import pandas as pd
-from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.preprocessing import StandardScaler
-from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
-from sklearn.metrics import accuracy_score, classification_report
-from sklearn.inspection import permutation_importance 
-from sklearn.svm import SVC
-from sklearn.tree import DecisionTreeClassifier, plot_tree
-from torchmetrics import ConfusionMatrix
-from mlxtend.plotting import plot_confusion_matrix
-from tqdm.auto import tqdm
+from sklearn.metrics import accuracy_score
+from sklearn.tree import DecisionTreeClassifier
 import matplotlib
-import matplotlib.pyplot as plt
-import seaborn as sns
-import json
+from executorch.exir import to_edge_transform_and_lower
+from executorch.backends.xnnpack.partition.xnnpack_partitioner import XnnpackPartitioner
+import sk2torch
 matplotlib.use('Agg')
 
 
-# dataset = pd.read_csv(r"/mnt/c/Users/Ziono/OneDrive/Documents/Garden IoT project/Garden IoT/pytorch/data/plant_health_data(1).csv") # Windows path
-dataset = pd.read_csv(r"/home/zoino/Documents/Projects/Garden-IoT/pytorch/data/plant_health_data(1).csv")
+dataset = pd.read_csv(r"/mnt/c/Users/Ziono/OneDrive/Documents/Garden IoT project/Garden IoT/pytorch/data/plant_health_data(1).csv") # Windows path
+# dataset = pd.read_csv(r"/home/zoino/Documents/Projects/Garden-IoT/pytorch/data/plant_health_data(1).csv")
 # Convert str data type values to category
 convert_cols = ["Plant_Health_Status"]
 dataset[convert_cols] = dataset[convert_cols].apply(lambda x: x.astype('category'))
@@ -75,5 +66,30 @@ best_rf_gini = grid_search_gini.best_estimator_
 
 best_target_pred = best_rf_gini.predict(X_test)
 
+print(f"Input: {X_test}\n Insput Shape: {X_test.shape}")
+
 print("Accuracy:",accuracy_score(y_test, best_target_pred))
-eval = best_rf_gini.eval()
+
+gini_pytorch_model = sk2torch.wrap(best_rf_gini)
+gini_pytorch_model.eval()
+
+example_inputs = (torch.zeros(240,6),)
+exported_program = torch.export.export(gini_pytorch_model, example_inputs)
+
+# Export to a basic, unoptimized edge dialect  for bare-metal MCU
+edge_manager = to_edge_transform_and_lower(exported_program)
+
+# Convert to ExecuTorch program with portable kernels
+executorch_program = edge_manager.to_executorch()
+
+# 3. Save for deployment
+with open("models/model_ptes/gini_model.pte", "wb") as f:
+    f.write(executorch_program.buffer)
+
+"""
+Microcontrollers like the RP2040 load models directly from flash memory 
+    rather than a file system. Use a terminal command to convert your .pte 
+    file into a C header array:
+    bash > xxd -i "models/model_ptes/gini_model.pte" > rp2040-freertos/lib/peripheral-libs/gini_model.h
+"""
+
